ea673bb828e28ad0a3caa43b1008b45d1ea7421d
James Wong <116855757+JamesW0803@users.noreply.github.com>
2023-04-06 17:09:30 +0800

    Add files via upload

---
+++ ifElseExercise1/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String choice = " ";
+
+        while(true){
+            System.out.println("Enter \"quit\" to quit the calculator. ");
+            choice = sc.next();
+            if (choice.equalsIgnoreCase("quit")){
+                break;
+            }
+            else{
+                System.out.println("Enter num1 operator num2 (example: 1 + 2): ");
+
+                double d1 = sc.nextDouble();
+                char op = sc.next().charAt(0);
+                double d2 = sc.nextDouble();
+
+                if (op == '+'){
+                    System.out.println((int) d1 + d2);
+                }
+                else if (op == '-'){
+                    System.out.println((int) d1 - d2);
+                }
+                else if (op == '*'){
+                    System.out.println((int) d1 * d2);
+                }
+                else if (op == '/'){
+                    System.out.println(d1 / d2);
+                }
+                else{
+                    System.out.println("Error...Please type the operators correctly...");
+                }
+            }
+        }
+    }
+}
+
+
