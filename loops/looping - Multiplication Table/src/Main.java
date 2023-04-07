b0677163c46ecf9182c5a5e8195ed674338d65cb
James Wong <jwyn0803@gmail.com>
2023-04-07 15:06:45 +0800

    Add files via upload

---
+++ loops/looping - Multiplication Table/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+
+        System.out.println("Enter the number range for the multiplication table: ");
+        num = sc.nextInt();
+
+        for (int i = 1; i <= num; i++){
+            for (int j = 1; j <= num; j++){
+                System.out.print((i * j) + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
